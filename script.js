7379d8362f233e14f98d6228af53bc210917a92e
M-Ehthisham18 <ehthishamulhaq18@gamil.com>
2024-10-06 16:34:47 +0530

    flicker down scroll button add

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const toggle = document.querySelector('.mobile-toggle');
 toggle.addEventListener('click',()=>{
     document.querySelector("#menu").classList.toggle("active");
 })
+    
+const scrollDownBtn = document.getElementById("scrollDownBtn");
+
+    // Show the button only at the top of the page
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 70) {
+            scrollDownBtn.classList.add("hide");
+        } else {
+            scrollDownBtn.classList.remove("hide");
+        }
+    });
+
+    // Scroll down to a section (you can modify this part based on your page layout)
+    scrollDownBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: window.innerHeight,
+            behavior: "smooth",
+        });
+    });
